7cbaaade7a964b9d47925930a9b6d1621ed39d5b
Omega <wpb911@gmail.com>
2021-01-26 03:13:08 -0500

    updated process.env.Jawsdb_url in connection.js heroku deploy.

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "mysqlroot",
     database: "burgers_db"
   });
-  
+};
+
 connection.connect(function(err) {
     if (err) {
         console.error("error connecting: " + err.stack);
